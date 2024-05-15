b39c9d40ef9fbc4bfb312d8bb63c0dc11d8ab125
Vasiliy Platon <vasia.platon@gmail.com>
2024-05-15 22:29:24 +0300

    fixes db depencies

--- app/entities/car/controller.py
+++ app/entities/car/controller.py
@@ -5,38 +5,32 @@ from ...database import SessionLocal
 
 class Car:
     model = models.Car
-    db = SessionLocal()
 
-    def __init__(self, id_got: int):
-        car = self.db.query(self.model).filter(self.model.id == id_got).first()
+    def __init__(self, id_got: int, db: SessionLocal):
+        car = db.query(self.model).filter(self.model.id == id_got).first()
         if not car:
             raise NotFound()
 
         self.db_entity: models.Car = car
 
     @classmethod
-    def create(cls, schema: schemas.CarCreate):
+    def create(cls, schema: schemas.CarCreate, db: SessionLocal):
         # TODO validate
         db_car = cls.model(
             **schema.dict()
         )
 
-        cls.db.add(db_car)
-        cls.db.commit()
-        cls.db.refresh(db_car)
-        return cls(db_car.id)
-
-    def _update(self):
-        self.db.refresh(self.db_entity)
+        db.add(db_car)
+        db.commit()
+        db.refresh(db_car)
+        return cls(db_car.id, db)
 
     @property
     def schema(self) -> schemas.CarReturn:
-        self._update()
-
         return schemas.CarReturn(**self.db_entity.__dict__)
 
     @classmethod
-    def found_for_user(cls, user_id: int) -> list[schemas.CarReturn]:
-        cars = cls.db.query(cls.model).filter(cls.model.user_id == user_id).all()
-        cars_got = [cls(car.id).schema for car in cars]
+    def found_for_user(cls, user_id: int, db: SessionLocal) -> list[schemas.CarReturn]:
+        cars = db.query(cls.model).filter(cls.model.user_id == user_id).all()
+        cars_got = [cls(car.id, db).schema for car in cars]
         return cars_got
--- app/entities/car/router.py
+++ app/entities/car/router.py
@@ -1,10 +1,12 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 from . import schemas
 from . import controller
 from ..user.controller import User
+from ...dependencies import get_db
 
 car_router = APIRouter(
     prefix="/cars",
@@ -14,18 +16,20 @@ car_router = APIRouter(
 
 @car_router.post("/")
 async def create_car(schema: schemas.CarGot,
-                     current_user: Annotated[User, Depends(User.get_current_user)]) -> schemas.CarReturn:
+                     current_user: Annotated[User, Depends(User.get_current_user)],
+                     db: Session = Depends(get_db)) -> schemas.CarReturn:
     """Добавляем машину пользователю"""
     return controller.Car.create(schemas.CarCreate(**schema.dict(), user_id=current_user.schema.id)).schema
 
 
 @car_router.get("/me")
-async def get_my_cars(current_user: Annotated[User, Depends(User.get_current_user)]) -> list[schemas.CarReturn]:
+async def get_my_cars(current_user: Annotated[User, Depends(User.get_current_user)],
+                      db: Session = Depends(get_db)) -> list[schemas.CarReturn]:
     """Получаем текущего машины пользователя"""
-    return controller.Car.found_for_user(current_user.schema.id)
+    return controller.Car.found_for_user(current_user.schema.id, db)
 
 
 @car_router.get("/user/{user_id}")
-async def get_for_user(user_id: int):
+async def get_for_user(user_id: int, db: Session = Depends(get_db)):
     """Получаем машины пользователя"""
-    return controller.Car.found_for_user(user_id)
+    return controller.Car.found_for_user(user_id, db)
