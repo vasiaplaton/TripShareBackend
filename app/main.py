91b22b9df2482914c63732db05590948a30b6848
Vasiliy Platon <vasia.platon@gmail.com>
2024-05-15 21:20:17 +0300

    cars fixed

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.entities.car.router import car_router
 from app.entities.chat.router import chat_router
 from app.entities.images.router import image_router
+from app.entities.request.router import request_router
 from app.entities.trip.router import trip_router
 from app.entities.user.router import user_router
 
@@ -14,3 +15,4 @@ app.include_router(trip_router)
 app.include_router(car_router)
 app.include_router(image_router)
 app.include_router(chat_router)
+app.include_router(request_router)
